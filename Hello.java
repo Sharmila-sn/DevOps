396fc59b546bc19942e30aee98df7787d25724d7
Sharmila-sn <112241655+Sharmila-sn@users.noreply.github.com>
2023-01-03 15:58:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
-public class Main
-{
-  public static void main(String args[])
-  {
-    System.out.println("MCA department");
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
   }
 }
